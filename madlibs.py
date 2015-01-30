6dfeb5f111f69b07036195c9a93a76abb24b52f4
Hackbright Student <info@hackbrightacademy.com>
2015-01-29 16:56:40 -0800

    working madlibs game with a number of different form inputs.

--- madlibs.py
+++ madlibs.py
@@ -28,6 +28,37 @@ def greet_person():
 
     return render_template("compliment.html", person=player, compliment=compliment)
 
+@app.route('/game')
+def show_game_form():
+    playing = request.args.get("play")
+    if playing == "no":
+        return  render_template("goodbye.html")
+    else: #if playing is 'yes'
+        return render_template("game.html")
+
+@app.route("/madlib")
+def show_madlib():
+    name = request.args.get("person_name")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adj = request.args.get("adj")
+    startdate = request.args.get("start_date")
+    coolPic = request.args.get("image")
+    cheese = request.args.get("cheese")
+    rabbit = request.args.get("rabbit")
+    chopsticks = request.args.get("chopsticks")
+
+    check_dict = {"a block cheese": cheese, "your lucky rabbit foot": rabbit, "a pair of chopsticks": chopsticks}
+    list_to_print = []
+
+    for key in check_dict.keys():
+        if check_dict[key] == 'on':
+            list_to_print.append(key)
+
+
+    return render_template("madlib.html", name=name, color=color, noun=noun, adj=adj, startdate=startdate, coolPic=coolPic, list_to_print=list_to_print)
+
+
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
